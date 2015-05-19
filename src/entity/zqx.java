81bd3747c596a18158af92fe5a6508769d4b6b22
zhaoqiuxing <87735866@qq.com>
2015-05-20 00:05:16 +0800

    Update zqx.java

--- src/entity/zqx.java
+++ src/entity/zqx.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class zqx  implements Serializable {
 
 	private static final long serialVersionUID = 7881945592591710454L;
-	private int iaa11zqxfsdfsddddds;
+	private int iaa11zqxfsdfsddddd1111111111s;
 	private Long id;
 	private String testformult;
 	private Set set =new HashSet();
